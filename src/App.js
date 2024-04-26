c4132efec48dd5db6416d753bfe36928cb4cb77a
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2024-04-26 22:50:59 +0700

    update final App.js

--- src/App.js
+++ src/App.js
@@ -1,110 +1,134 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
 
-  const urlProv = "https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"
-  const [prov, setProv] = useState([])
-  useEffect(() => {
-    lihatProvinsi()
-  }, [])
-  const lihatProvinsi = () => {
-    axios.get(urlProv).then(function (response) {
-      console.log(response.data)
-      setProv(response.data)
-    })
-  }
-
-
-  const provinsi = 11
-  const urlKab = `https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${provinsi}.json`
-  const [kab, setKab] = useState([])
+  const [provinsi, setProvinsi] = useState([]);
+  const [kabupaten, setKabupaten] = useState([]);
+  const [kecamatan, setKecamatan] = useState([]);
+  const [kelurahan, setKelurahan] = useState([]);
+  const [selectedProvinceCode, setSelectedProvinceCode] = useState('');
+  const [selectedProvinceName, setSelectedProvinceName] = useState('');
+  const [selectedRegenciesCode, setSelectedRegenciesCode] = useState('');
+  const [selectedRegenciesName, setSelectedRegenciesName] = useState('');
+  const [selectedDistrictsCode, setSelectedDistrictsCode] = useState('');
+  const [selectedDistrictsName, setSelectedDistrictsName] = useState('');
 
   useEffect(() => {
-    lihatKabupaten()
-  }, [])
+    // Ambil data dari API provinsi
+    axios.get('https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json')
+      .then(response => {
+        setProvinsi(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching provinces:', error);
+      });
+  }, []);
 
-  const lihatKabupaten = () => {
-    axios.get(urlKab).then(function (response) {
-      console.log(response.data)
-      setKab(response.data)
-    })
-  }
+  // Handle perubahan option provinsi
+  const handleProvinceChange = (e) => {
+    const selectedProvinceCode = e.target.value;
+    const selectedProvinceName = e.target.options[e.target.selectedIndex].text;
+    setSelectedProvinceCode(selectedProvinceCode);
+    setSelectedProvinceName(selectedProvinceName);
 
+    // Reset kosong kecamatan dan kelurahan
+    setKecamatan([]);
+    setKelurahan([]);
 
-  const kabupaten = 1103
-  const urlKec = `https://www.emsifa.com/api-wilayah-indonesia/api/districts/${kabupaten}.json`
-  const [kec, setKec] = useState([])
+    // Ambil data kabupaten dari provinsi yang dipilih
+    axios.get(`https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${selectedProvinceCode}.json`)
+      .then(response => {
+        setKabupaten(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching regencies:', error);
+      });
+  };
 
-  useEffect(() => {
-    lihatKecamatan()
-  }, [])
+  // Handle perubahan option kabupaten
+  const handleRegenciesChange = (r) => {
+    const selectedRegenciesCode = r.target.value;
+    const selectedRegenciesName = r.target.options[r.target.selectedIndex].text;
+    setSelectedRegenciesCode(selectedRegenciesCode);
+    setSelectedRegenciesName(selectedRegenciesName);
 
-  const lihatKecamatan = () => {
-    axios.get(urlKec).then(function (response) {
-      console.log(response.data)
-      setKec(response.data)
-    })
-  }
+    // Reset kelurahan
+    setKelurahan([]);
 
-  const kecamatan = 1103010
-  const urlKel = `https://www.emsifa.com/api-wilayah-indonesia/api/villages/${kecamatan}.json`
-  const [kel, setKel] = useState([])
+    // Ambil data kecamatan dari kabupaten yang dipilih
+    axios.get(`https://www.emsifa.com/api-wilayah-indonesia/api/districts/${selectedRegenciesCode}.json`)
+      .then(response => {
+        setKecamatan(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching districts:', error);
+      });
+  };
 
-  useEffect(() => {
-    lihatKelurahan()
-  }, [])
+  // Handle perubahan kecamatan
+  const handleDistrictsChange = (d) => {
+    const selectedDistrictsCode = d.target.value;
+    const selectedDistrictsName = d.target.options[d.target.selectedIndex].text;
+    setSelectedDistrictsCode(selectedDistrictsCode);
+    setSelectedDistrictsName(selectedDistrictsName);
 
-  const lihatKelurahan = () => {
-    axios.get(urlKel).then(function (response) {
-      console.log(response.data)
-      setKel(response.data)
-    })
-  }
+    // Ambil data kelurahan dari kecamatan yang dipilih
+    axios.get(`https://www.emsifa.com/api-wilayah-indonesia/api/villages/${selectedDistrictsCode}.json`)
+      .then(response => {
+        setKelurahan(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching villages:', error);
+      });
+  };
 
   return (
     <div>
-      <h2>Pilih provinsi</h2>
-      <select>
-        <option value="">Pilih provinsi</option>
-        {
-          prov.map((p) => (
-            <option key={p.id} value={p.id}>{p.name}</option>
-          ))
-        }
+      <center>
+      <h1>Soal 2 - Get Data From API</h1>
+      </center>
+      <label>Pilih Provinsi:    </label>
+      <select onChange={handleProvinceChange} value={selectedProvinceCode}>
+        <option value="">{selectedProvinceName ? selectedProvinceName : 'Pilih Provinsi'}</option>
+        {provinsi.map(prov => (
+          <option key={prov.id} value={prov.id}>
+            {prov.name}
+          </option>
+        ))}
       </select>
-      <br/>
-      <h2>Pilih kabupaten</h2>
-      <select>
-        <option value="">Pilih kabupaten</option>
-        {
-          kab.map((k) => (
-            <option key={k.id} value={k.id}>{k.name}</option>
-          ))
-        }
+      <br/><br/>
+      <label>Pilih Kabupaten:    </label>
+      <select onChange={handleRegenciesChange} value={selectedRegenciesCode}>
+        <option value="">{selectedRegenciesName ? selectedRegenciesName : 'Pilih Kabupaten'}</option>
+        {kabupaten.map(kab => (
+          <option key={kab.id} value={kab.id}>
+            {kab.name}
+          </option>
+        ))}
       </select>
-      <br/>
-      <h2>Pilih kecamatan</h2>
-      <select>
-        <option value="">Pilih kecamatan</option>
-        {
-          kec.map((c) => (
-            <option key={c.id} value={c.id}>{c.name}</option>
-          ))
-        }
+      <br/><br/>
+      <label>Pilih Kecamatan:    </label>
+      <select onChange={handleDistrictsChange} value={selectedDistrictsCode}>
+        <option value="">{selectedDistrictsName ? selectedDistrictsName : 'Pilih Kecamatan'}</option>
+        {kecamatan.map(kec => (
+          <option key={kec.id} value={kec.id}>
+            {kec.name}
+          </option>
+        ))}
       </select>
-      <br/>
-      <h2>Pilih kelurahan</h2>
+      <br/><br/>
+      <label>Pilih Kelurahan:    </label>
       <select>
-        <option value="">Pilih kelurahan</option>
-        {
-          kel.map((l) => (
-            <option key={l.id} value={l.id}>{l.name}</option>
-          ))
-        }
+        <option value="">Pilih Kelurahan</option>
+        {kelurahan.map(kel => (
+          <option key={kel.id} value={kel.id}>
+            {kel.name}
+          </option>
+        ))}
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
