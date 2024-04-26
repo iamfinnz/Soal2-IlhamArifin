ae05ac4ffc2e63425a4ec230cf4cd46b6599b2b0
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2024-04-26 13:56:30 +0700

    update code

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const urlProv =  "https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"
-
-export const getProvinsi = async() => {
-    const movie = await axios.get(urlProv)
-    return movie.data
-}
